a1d5bef148233554c1bcf70b3bdd6d319e1fc2b6
JoeLoera <joeloera7@gmail.com>
2023-02-03 06:51:45 -0800

    Coded js for ships being able to flip both ways

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const optionContainer = document.querySelector(".option-container");
 const flipButton = document.querySelector("#flip-button");
 
+let angle = 0;
 function flip() {
   const optionShips = Array.from(optionContainer.children);
+  //IF statement for flipping the ships
+  // if (angle === 0) {
+  //   angle = 90;
+  // } else {
+  //   angle = 0;
+  // }
+
+  //More advanced way of getting the ships to flip without an IF statement
+  angle = angle === 0 ? 90 : 0;
   optionShips.forEach(
-    (optionShip) => (optionShip.style.transform = `rotate(90deg)`)
+    (optionShip) => (optionShip.style.transform = `rotate(${angle}deg)`)
   );
 }
 flipButton.addEventListener("click", flip);
