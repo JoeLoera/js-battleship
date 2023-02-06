adc72dd63b2af32c8e09cb0d49272e54e0f8afb5
JoeLoera <joeloera7@gmail.com>
2023-02-06 07:22:12 -0800

    creating 100 divs

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+const gamesBoardContainer = document.querySelector("#gamesboard-container");
 const optionContainer = document.querySelector(".option-container");
 const flipButton = document.querySelector("#flip-button");
 
-//option chooseing
+//option choosing
 let angle = 0;
 function flip() {
   const optionShips = Array.from(optionContainer.children);
@@ -19,6 +20,64 @@ function flip() {
   );
 }
 
-//Creating boards
-
 flipButton.addEventListener("click", flip);
+
+//Creating boards
+const width = 10;
+
+function createBoard(color, user) {
+  const gameBoardContainer = document.createElement("div");
+  gameBoardContainer.classList.add("game-board");
+  gameBoardContainer.style.backgroundColor = color;
+  gameBoardContainer.id = user;
+
+  for (let i = 0; i < width * width; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.id = i;
+    gameBoardContainer.append(block);
+  }
+
+  gamesBoardContainer.append(gameBoardContainer);
+}
+
+createBoard("yellow", "player");
+createBoard("pink", "computer");
+
+//Creating Ships
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+  }
+}
+
+const destroyer = new Ship("destroyer", 2);
+const submarine = new Ship("submarine", 3);
+const cruiser = new Ship("cruiser", 3);
+const battleship = new Ship("battleship", 4);
+const carrier = new Ship("carrier", 5);
+
+const ships = [destroyer, submarine, cruiser, battleship, carrier];
+
+function addShipPiece(ship) {
+  const allBoardBlocks = document.querySelectorAll("#computer div");
+  let randomBoolean = Math.random() < 0.5;
+  let isHorizontal = randomBoolean;
+  let randomStartIndex = Math.floor(Math.random() * width * width);
+
+  console.log(randomStartIndex);
+
+  let shipBlocks = [];
+
+  for (let i = 0; i < ship.length; i++) {
+    if (isHorizontal) {
+      shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i]);
+    } else {
+      shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i * width]);
+    }
+  }
+  console.log(shipBlocks);
+}
+
+addShipPiece(destroyer);
