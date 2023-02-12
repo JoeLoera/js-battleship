9965801328648fe56f5f910c65badd95a33aa40b
JoeLoera <joeloera7@gmail.com>
2023-02-12 12:14:05 -0800

    Fixed the drag function. Mispelled dragStart

--- app.js
+++ app.js
@@ -59,20 +59,24 @@ const battleship = new Ship("battleship", 4);
 const carrier = new Ship("carrier", 5);
 
 const ships = [destroyer, submarine, cruiser, battleship, carrier];
+let notDropped;
 
-function addShipPiece(ship) {
-  const allBoardBlocks = document.querySelectorAll("#computer div");
+function addShipPiece(user, ship, startId) {
+  const allBoardBlocks = document.querySelectorAll(`#${user} div`);
   let randomBoolean = Math.random() < 0.5;
-  let isHorizontal = randomBoolean;
+  let isHorizontal = user === "player" ? angle === 0 : randomBoolean;
   let randomStartIndex = Math.floor(Math.random() * width * width);
 
+  let startIndex = startId ? startId : randomStartIndex;
+
   let validStart = isHorizontal
-    ? randomStartIndex <= width * width - ship.length
-      ? randomStartIndex
+    ? startIndex <= width * width - ship.length
+      ? startIndex
       : width * width - ship.length
-    : randomStartIndex <= width * width - width * ship.length
-    ? randomStartIndex
-    : randomStartIndex - ship.length * width + width;
+    : //handle vertical
+    startIndex <= width * width - width * ship.length
+    ? startIndex
+    : startIndex - ship.length * width + width;
 
   let shipBlocks = [];
 
@@ -110,17 +114,18 @@ function addShipPiece(ship) {
       shipBlock.classList.add("taken");
     });
   } else {
-    addShipPiece(ship);
+    if (user === "computer") addShipPiece("computer", ship);
+    if (user === "player") notDropped = true;
   }
 }
 
-ships.forEach((ship) => addShipPiece(ship));
+ships.forEach((ship) => addShipPiece("computer", ship));
 
 //Drag player ships
 let draggedShip;
 const optionShips = Array.from(optionContainer.children);
 optionShips.forEach((optionShip) =>
-  optionShip.addEventListener("dragstart", dragstart)
+  optionShip.addEventListener("dragstart", dragStart)
 );
 
 const allPlayerBlocks = document.querySelectorAll("#player div");
@@ -130,6 +135,7 @@ allPlayerBlocks.forEach((playerBlock) => {
 });
 
 function dragStart(e) {
+  notDropped = false;
   draggedShip = e.target;
 }
 
@@ -137,7 +143,11 @@ function dragOver(e) {
   e.preventDefault();
 }
 
-function dropShip() {
+function dropShip(e) {
   const startId = e.target.id;
   const ship = ships[draggedShip.id];
+  addShipPiece("player", ship, startId);
+  if (!notDropped) {
+    draggedShip.remove();
+  }
 }
